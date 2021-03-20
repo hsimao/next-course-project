1f3b9a231e4bfb1ce9108a5308e36f57e2be2dc5
hsimao <e087754958@gmail.com>
2021-03-20 15:37:15 +0800

    add: markdown render code, use syntax-highlighter

--- components/posts/postDetail/postContent.js
+++ components/posts/postDetail/postContent.js
@@ -1,3 +1,5 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./postHeader";
 import Image from "next/image";
@@ -27,6 +29,16 @@ export default function PostContent(props) {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { language, value } = code;
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={value}
+        />
+      );
     }
   };
 
