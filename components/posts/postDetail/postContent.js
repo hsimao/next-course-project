d11fc46fa2faa5cf1c03bfa4dd4e37dcae062000
hsimao <e087754958@gmail.com>
2021-03-20 15:27:28 +0800

    add: custom Markdown render

--- components/posts/postDetail/postContent.js
+++ components/posts/postDetail/postContent.js
@@ -1,15 +1,39 @@
 import ReactMarkdown from "react-markdown";
 import PostHeader from "./postHeader";
+import Image from "next/image";
 import classes from "./postContent.module.css";
 
 export default function PostContent(props) {
   const { slug, title, image, content } = props.post;
   const imagePath = `/images/posts/${slug}/${image}`;
 
+  // 將 Markdown 的 img 替換成 next Image 組件來顯示
+  const customRenterers = {
+    paragraph(paragraph) {
+      const { node } = paragraph;
+
+      // fix ReactMarkdown error (HTML to contain a matching <div> in <p>.)
+      if (node.children[0].type === "image") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${slug}/${image.url}`}
+              alt={title}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    }
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader title={title} image={imagePath} />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown renderers={customRenterers}>{content}</ReactMarkdown>
     </article>
   );
 }
