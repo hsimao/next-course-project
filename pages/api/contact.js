622a7b4d550c91b4817305c76d85f7bc2be29b98
hsimao <e087754958@gmail.com>
2021-03-21 16:32:46 +0800

    add: connect mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,3 +1,7 @@
+import { MongoClient } from "mongodb";
+
+const mongodbConnectUrl = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.iekg0.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
 function isInvalidParams({ email, name, message }) {
   return (
     !email ||
@@ -9,7 +13,7 @@ function isInvalidParams({ email, name, message }) {
   );
 }
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method === "POST") {
     const { email, name, message } = req.body;
     if (isInvalidParams({ email, name, message })) {
@@ -24,9 +28,26 @@ export default function handler(req, res) {
       name,
       message
     };
+    let client;
+    try {
+      client = await MongoClient.connect(mongodbConnectUrl);
+    } catch (err) {
+      res.status(500).json({ message: "Could not connect to database." });
+      return;
+    }
 
-    console.log(newMessage);
+    const db = client.db();
 
+    try {
+      const result = await db.collection("message").insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (err) {
+      client.close();
+      res.status(500).json({ message: "Storing message filed!" });
+      return;
+    }
+
+    client.close();
     res.status(201).json({
       message: "Successfully stored message!",
       data: newMessage
